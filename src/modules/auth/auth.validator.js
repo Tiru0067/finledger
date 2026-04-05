03ee47a270ca91665418cecd7448b1f41aee1acb
tiru0067 <tiru0067@gmail.com>
2026-04-05 18:29:16 +0530

    fix: add empty body validation to login endpoint

--- src/modules/auth/auth.validator.js
+++ src/modules/auth/auth.validator.js
@@ -1,6 +1,10 @@
 import AppError from "../../utils/AppError.js";
 
 export const validateLogin = (req, res, next) => {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return next(new AppError("Request body is required", 400));
+  }
+
   const { email, password } = req.body;
 
   // validate missing fields
